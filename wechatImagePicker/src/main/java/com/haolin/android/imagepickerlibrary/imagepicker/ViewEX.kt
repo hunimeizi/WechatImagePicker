fa8310a4952e9d13989580b6d8a055441e6a83dc
yunboAndroid <lhaolin0304@aliyun.com>
2022-12-16 11:04:48 +0800

    Update ViewEX.kt

--- wechatImagePicker/src/main/java/com/haolin/android/imagepickerlibrary/imagepicker/ViewEX.kt
+++ wechatImagePicker/src/main/java/com/haolin/android/imagepickerlibrary/imagepicker/ViewEX.kt
@@ -11,7 +11,7 @@ internal fun FragmentActivity.PermissionRequest(
     PermissionX.init(this)
         .permissions(permissions)
         .onForwardToSettings { scope, deniedList -> //拒绝且不再询问
-            scope.showForwardToSettingsDialog(deniedList, "请在设置中允许以下权限", "去允许", "取消")
+            scope.showRequestReasonDialog(deniedList, "请在设置中允许以下权限", "去允许", "取消")
         }.request { allGranted, _, _ ->
             if(allGranted) {
                 permissionSuccess()
